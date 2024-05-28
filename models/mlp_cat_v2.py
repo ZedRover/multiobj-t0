feat: fix prior loss calculation issue in CatNet forward pass

models/mlp_cat_v2.py
@@ -84,7 +84,7 @@ class CatNet(pl.LightningModule):
             act=act,
         )
         self._weight_init()
-        self.prior_losses = None 
+        self.prior_losses = None
 
     def _weight_init(self):
         for m in self.modules():
@@ -97,30 +97,42 @@ class CatNet(pl.LightningModule):
         optimizer = optim.AdamW(
             self.parameters(), lr=self.lr, weight_decay=self.weight_decay
         )
-        # scheduler = optim.lr_scheduler.LinearLR(
-        #     optimizer, start_factor=1, end_factor=0.1, total_iters=120
-        # )
+        scheduler = optim.lr_scheduler.LinearLR(
+            optimizer, start_factor=1, end_factor=0.1, total_iters=120
+        )
         scheduler = optim.lr_scheduler.CosineAnnealingLR(
-            optimizer, T_max=10, eta_min=0.1
+            optimizer, T_max=20, eta_min=0.1
+        )
+        scheduler = optim.lr_scheduler.OneCycleLR(
+            optimizer,
+            epochs=200,
+            steps_per_epoch=148,
+            max_lr=self.lr * 2,
+            three_phase=True,
         )
         return [optimizer], [scheduler]
 
     def forward(self, x):
         return self.model(x)
 
-    def _get_reconstruction_loss(self, outputs, y):
+    def _get_reconstruction_loss(self, outputs, y, stage=None):
         mid_output, last_output = outputs
         loss = th.tensor(0.0, device=y.device)
-        # var = y.std(dim=0)
-        # print(f"var:{var}")
         for i, weight in enumerate(self.loss_weights):
-            loss_i = weight * self.loss_fn(mid_output[:, i], y[:, i])
-            # print(f"loss-{i}:{loss_i:.2f}")
-            print(loss_i)
-            loss += loss_i
+            loss_i = self.loss_fn(mid_output[:, i], y[:, i])
+            normalized_loss_i = loss_i / (self.prior_losses[i])
+            weighted_normloss_i = weight * normalized_loss_i
+            if stage == "train":
+                self.log(f"train/loss_{i}", loss_i)
+                self.log(f"train/loss_norm_{i}", normalized_loss_i)
+                self.log(f"train/loss_weighted_{i}", weighted_normloss_i)
+            loss += weighted_normloss_i
 
-        loss += self.loss_fn(last_output.flatten(), y[:, 0])
-        # add l2 regularization
+        loss += (
+            self.loss_fn(last_output.flatten(), y[:, 0])
+            * self.loss_weights[0]
+            / self.prior_losses[0]
+        )
         l2_reg = th.tensor(0.0, device=y.device)
         for name, param in self.named_parameters():
             if "weight" in name and "bn" not in name:
@@ -131,7 +143,7 @@ class CatNet(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         outputs = self.model(x)
-        loss = self._get_reconstruction_loss(outputs, y)
+        loss = self._get_reconstruction_loss(outputs, y, "train")
         met = calc_catmets(outputs, y, "train")
         self.log("train_loss", loss)
         self.log_dict(met)
@@ -156,17 +168,25 @@ class CatNet(pl.LightningModule):
         return loss
 
 
-def calculate_prior_losses(module, data_loader):
+def calculate_prior_losses(
+    module,
+    data_loader,
+    loss="mse",
+):
     prior_losses = []
     all_labels = []
     for batch in data_loader:
         _, labels = batch
         all_labels.append(labels)
     all_labels = th.cat(all_labels, dim=0)
-    mean_labels = th.mean(all_labels, dim=0)
+    mean_labels = th.mean(all_labels, dim=0, keepdim=True)
+    loss_fn = loss_fn_dict[loss]
 
     for i in range(len(module.loss_weights)):
-        prior_loss = th.mean((all_labels[:, i] - mean_labels[i]) ** 2).item()
+        # Ensure the target size matches the input size
+        target = mean_labels[:, i].expand_as(all_labels[:, i])
+        prior_loss = loss_fn(all_labels[:, i], target).item()
         prior_losses.append(prior_loss)
+    print("prior_losses: ", prior_losses)
 
     module.prior_losses = prior_losses